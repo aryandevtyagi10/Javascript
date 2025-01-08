30fb68f18a6a32a09893c45c39380d3f00c7a7cf
aryandevtyagi10 <168193058+aryandevtyagi10@users.noreply.github.com>
2025-01-08 09:18:27 +0530

    Add files via upload

---
+++ Basics/control statements/app.js
@@ -0,0 +1,27 @@
+// let a = 10;
+// let b = 10;
+// if (a>b){
+//     console.log("a is greater than b ");
+//     }
+//     else if (a<b){
+//         console.log("a is less than b ");
+//     }
+//     else {
+//         console.log("a and b are equal");
+//     }
+
+
+//  Exercise 
+let pass = 8;
+if (pass < 8){
+console.log("password is short");
+}
+else if (pass === 8) {
+    console.log("welcome");
+}
+else if(pass > 8){
+   console.log("password is too long &  must be 8 characters"); 
+}
+else {
+    console.log("please enter a password ");
+}
