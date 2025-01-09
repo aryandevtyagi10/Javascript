e492dd16627a0e408fec397fdaa561c60f921b46
aryandevtyagi10 <168193058+aryandevtyagi10@users.noreply.github.com>
2025-01-09 19:05:04 +0530

    Add files via upload

---
+++ Basics/array methods/app.js
@@ -0,0 +1,9 @@
+// arrays
+const arraynum =[1,2,3,4,[15,45,90],6];
+const str = ['a','aryan','dev'];
+// Accessing array 
+console.log(str[0]);
+console.log(arraynum[4][2]);//2 array
+
+let fb = ['hazard','messi'];
+console.log(fb[1]);+++++++++++++++++++++++++++++++++++
