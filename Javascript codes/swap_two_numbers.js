c73afdbc00cc9b39c5a6f201f500de5d4a2ad957
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-04-21 17:20:40 +0530

    Create swap_two_numbers.js

---
+++ Javascript codes/swap_two_numbers.js
@@ -0,0 +1,33 @@
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
+
+
+
+
+//Using es6(ES2015) Destructuring assignment
+
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//using destructuring assignment
+[a, b] = [b, a];
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
